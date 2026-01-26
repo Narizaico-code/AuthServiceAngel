b0127ada7597257be3e38800d3320bf2f0705cb9
Narizaico-code <areyes-2024435@kinal.edu.gt>
2026-01-26 17:16:58 -0600

    5. SUbido dia lunes

--- src/AuthServiceAngel.Application/obj/Debug/net8.0/AuthServiceAngel.Application.AssemblyInfo.cs
+++ src/AuthServiceAngel.Application/obj/Debug/net8.0/AuthServiceAngel.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServiceAngel.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26b1f34bae66e6dda657a30054fca0277adb21e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c8aaea76f5048a944380ac6a71bc9789ec6d18")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServiceAngel.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServiceAngel.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
